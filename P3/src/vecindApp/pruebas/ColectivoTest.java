15ffbb908f1d3f55d56887ed623a91b73719493f
Fgp910 <f.gutierrezpena910@gmail.com>
2020-03-18 21:26:47 +0100

    Prueba para hacer módulos de prueba

--- P3/src/vecindApp/pruebas/ColectivoTest.java
+++ P3/src/vecindApp/pruebas/ColectivoTest.java
@@ -1,13 +1,23 @@
 package vecindApp.pruebas;
 
+import org.junit.Before;
 import org.junit.Test;
+import vecindApp.clases.Ciudadano;
+import vecindApp.clases.Colectivo;
 
 import static org.junit.Assert.*;
 
 public class ColectivoTest {
+    Colectivo c;
+
+    @Before
+    public void setUp() {
+        c = new Colectivo("miColectivo", new Ciudadano("usr", "psswd", "12345678X"));
+    }
 
     @Test
     public void getNombre() {
+        assertEquals("miColectivo", c.getNombre());
     }
 
     @Test
