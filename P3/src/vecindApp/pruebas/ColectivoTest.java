f52979f7351695f183cd2e591c63e5989c0aceea
Fgp910 <f.gutierrezpena910@gmail.com>
2020-03-18 21:13:31 +0100

    Prueba para hacer módulos de prueba

---
+++ P3/src/vecindApp/pruebas/ColectivoTest.java
@@ -0,0 +1,56 @@
+package vecindApp.pruebas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColectivoTest {
+
+    @Test
+    public void getNombre() {
+    }
+
+    @Test
+    public void setNombre() {
+    }
+
+    @Test
+    public void getElementos() {
+    }
+
+    @Test
+    public void setElementos() {
+    }
+
+    @Test
+    public void getRepresentante() {
+    }
+
+    @Test
+    public void setRepresentante() {
+    }
+
+    @Test
+    public void getProyectos() {
+    }
+
+    @Test
+    public void setProyectos() {
+    }
+
+    @Test
+    public void addElemento() {
+    }
+
+    @Test
+    public void removeElemento() {
+    }
+
+    @Test
+    public void addProyecto() {
+    }
+
+    @Test
+    public void removeProyecto() {
+    }
+}
